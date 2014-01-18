31873654660543e082ef4b0f135c42f2e7296110
Eddie Burris <burrise@umkc.edu>
2014-01-17 18:53:26 -0600

    Added procedures

--- Hello.java
+++ Hello.java
@@ -1,5 +1,157 @@
-public class Hello {
+public class HelloClass {
 	public static void main(String[] args) {
 		System.out.println("Hello CS459");
+		f1();
+		f2();
+		f3();
+		f4();
+		f5();
+		f6();
+		f7();
+		f8();
+		f9();
+		f10();
+		f11();
+		f12();
+		f13();
+		f14();
+		f15();
+		f16();
+		f17();
+		f18();
+		f19();
+		f20();
+		f21();
+		f22();
+		f23();
+		f24();
+		f25();
+	}
+
+
+	private static void f25() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f24() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f23() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f22() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private static void f21() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f20() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f19() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f18() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f17() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f16() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f15() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f14() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f13() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f12() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f11() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f10() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f9() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f8() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f7() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void f1() {
+		// TODO Auto-generated method stub
+		
 	}
 }
